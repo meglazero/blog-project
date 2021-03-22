d5fcad10c933b49047ae9f449c1c133243b885ed
Butt <alexander.vanmeter@gmail.com>
2021-03-22 16:44:16 -0500

    Blog functional and built

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const mongo = require('mongoose');
+const Article = require('./models/article');
+const methodOverride = require('method-override');
+const app = express();
+
+mongo.connect('mongodb://localhost/blog', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true, 
+    useCreateIndex: true})
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+
+app.use('/articles', articleRouter);
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc' });
+    res.render('articles/index', { articles: articles });
+});
+
+app.listen(5000);
